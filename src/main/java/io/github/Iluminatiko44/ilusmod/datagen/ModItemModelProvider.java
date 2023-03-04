3a7f2135ccd0ba2c4beb120f37f882058c39b6fc
Iluminatiko44 <ben.uhl@icloud.com>
2023-03-04 17:44:44 +0100

    refactored the class with generics types

--- src/main/java/io/github/Iluminatiko44/ilusmod/datagen/ModItemModelProvider.java
+++ src/main/java/io/github/Iluminatiko44/ilusmod/datagen/ModItemModelProvider.java
@@ -22,47 +22,23 @@ public class ModItemModelProvider extends ItemModelProvider {
         simpleItem(ItemInit.GRAPES);
         simpleItem(ItemInit.POMMES);
         simpleItem(ItemInit.HAPPY_INGOT);
-        swordHandheldItem(ItemInit.HAPPY_SWORD);
-        axeHandheldItem(ItemInit.HAPPY_AXE);
-        pickaxeHandheldItem(ItemInit.HAPPY_PICKAXE);
-        shovelHandheldItem(ItemInit.HAPPY_SHOVEL);
-        hoeHandheldItem(ItemInit.HAPPY_HOE);
+        handheldItem(ItemInit.HAPPY_SWORD);
+        handheldItem(ItemInit.HAPPY_AXE);
+        handheldItem(ItemInit.HAPPY_PICKAXE);
+        handheldItem(ItemInit.HAPPY_SHOVEL);
+        handheldItem(ItemInit.HAPPY_HOE);
     }
 
+    @SuppressWarnings("UnusedReturnValue")
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
                         new ResourceLocation(Ilusmod.MODID, "item/" + item.getId().getPath()));
     }
 
-    private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/handheld")).texture("layer0",
-                new ResourceLocation(Ilusmod.MODID, "item/" + item.getId().getPath()));
-    }
-    // I don't know why, but it doesn't work with the Item class
-    //
-    private ItemModelBuilder swordHandheldItem(RegistryObject<SwordItem> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/handheld")).texture("layer0",
-                new ResourceLocation(Ilusmod.MODID, "item/" + item.getId().getPath()));
-    }
-    private ItemModelBuilder axeHandheldItem(RegistryObject<AxeItem> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/handheld")).texture("layer0",
-                new ResourceLocation(Ilusmod.MODID, "item/" + item.getId().getPath()));
-    }
-    private ItemModelBuilder pickaxeHandheldItem(RegistryObject<PickaxeItem> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/handheld")).texture("layer0",
-                new ResourceLocation(Ilusmod.MODID, "item/" + item.getId().getPath()));
-    }
-    private ItemModelBuilder shovelHandheldItem(RegistryObject<ShovelItem> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/handheld")).texture("layer0",
-                new ResourceLocation(Ilusmod.MODID, "item/" + item.getId().getPath()));
-    }
-    private ItemModelBuilder hoeHandheldItem(RegistryObject<HoeItem> item) {
+    // I just learned that this is a thing! (<T extends Item>)
+    @SuppressWarnings("UnusedReturnValue")
+    private <T extends Item> ItemModelBuilder handheldItem(RegistryObject<T> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/handheld")).texture("layer0",
                 new ResourceLocation(Ilusmod.MODID, "item/" + item.getId().getPath()));
