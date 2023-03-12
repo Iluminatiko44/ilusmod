6e6eeb2f7e47dcee1483d457c8e5543a4e8c380a
Iluminatiko44 <ben.uhl@icloud.com>
2023-03-12 21:32:42 +0100

    Added the happy ores to the worldgeneration

--- src/main/java/io/github/Iluminatiko44/ilusmod/worldGen/ModConfiguredFeatures.java
+++ src/main/java/io/github/Iluminatiko44/ilusmod/worldGen/ModConfiguredFeatures.java
@@ -7,29 +7,56 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.ThreeLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.AcaciaFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.UpwardsBranchingTrunkPlacer;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import net.minecraftforge.common.Tags;
 
+import java.util.List;
 import java.util.OptionalInt;
 
 public class ModConfiguredFeatures {
 
     public static final ResourceKey<ConfiguredFeature<?, ?>> HAPPY_KEY = registerKey("happy");
 
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_HAPPY_ORE_KEY = registerKey("happy_ore");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> END_HAPPY_ORE_KEY = registerKey("end_happy_ore");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> NETHER_HAPPY_ORE_KEY = registerKey("nether_happy_ore");
+
     // TODO: Understand Trees...
     // TODO: Understand the holder set parameter for the happy_leaves (currently Tags.Block.ORES)
     public static void bootstrap(BootstapContext< ConfiguredFeature<?, ?> > context) {
         HolderGetter<Block> holdergetter = context.lookup(Registries.BLOCK);
+        RuleTest stoneReplaceable = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplaceable = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest netherrackReplaceable = new BlockMatchTest(Blocks.NETHERRACK);
+        RuleTest endstoneReplaceable = new BlockMatchTest(Blocks.END_STONE);
+
+        List<OreConfiguration.TargetBlockState> overworldHappyOres =
+                List.of(
+                        OreConfiguration.target(stoneReplaceable, BlockInit.HAPPY_ORE.get().defaultBlockState()),
+                        OreConfiguration.target(deepslateReplaceable, BlockInit.DEEPSLATE_HAPPY_ORE.get().defaultBlockState())
+                );
+                                                                                                                  // this is the oreSize in Blocks
+        register(context, OVERWORLD_HAPPY_ORE_KEY, Feature.ORE, new OreConfiguration(overworldHappyOres, 16));
+        register(context, END_HAPPY_ORE_KEY, Feature.ORE, new OreConfiguration(endstoneReplaceable, BlockInit.ENDSTONE_HAPPY_ORE.get().defaultBlockState(), 8));
+        register(context, NETHER_HAPPY_ORE_KEY, Feature.ORE, new OreConfiguration(netherrackReplaceable, BlockInit.NETHERRACK_HAPPY_ORE.get().defaultBlockState(), 12));
+
+        // Tree
         register(
                 context, HAPPY_KEY, Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(BlockInit.HAPPY_LOG.get()),
--- src/main/java/io/github/Iluminatiko44/ilusmod/worldGen/ModPlacedFeatures.java
+++ src/main/java/io/github/Iluminatiko44/ilusmod/worldGen/ModPlacedFeatures.java
@@ -10,7 +10,9 @@ import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 
@@ -20,6 +22,11 @@ public class ModPlacedFeatures {
 
     public static final ResourceKey<PlacedFeature> HAPPY_PLACED_KEY = createKey("happy_placed");
 
+    public static final ResourceKey<PlacedFeature> OVERWORLD_HAPPY_ORE_PLACED_KEY = createKey("happy_ore_placed");
+    public static final ResourceKey<PlacedFeature> END_HAPPY_ORE_PLACED_KEY = createKey("end_happy_ore_placed");
+    public static final ResourceKey<PlacedFeature> NETHER_HAPPY_PLACED_ORE_KEY = createKey("nether_happy_ore_placed");
+
+
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?> > configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
@@ -28,6 +35,18 @@ public class ModPlacedFeatures {
                 // PlacementUtils: 1: count, 2: chance (?) (Also the reciprocal needs to be an int, so 0.1, 0.2, 0.25 but not 0.3 for example), 3: extra;    The Block is a filter for where the sapling tree can be placed
                 VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2), BlockInit.HAPPY_SAPLING.get())
         );
+
+        register(context, OVERWORLD_HAPPY_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_HAPPY_ORE_KEY),
+                                                // 1: Veins/chunks, 2: The height range from the bottom to y+60, HRPlacement.uniform: uniform distribution, triangle: Deeper down is higher chance
+                ModOrePlacement.commonOrePlacement(10, HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(0), VerticalAnchor.absolute(50)))
+        );
+        register(context, END_HAPPY_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_HAPPY_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(2, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.belowTop(0)))
+        );
+        register(context, NETHER_HAPPY_PLACED_ORE_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.NETHER_HAPPY_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(12, HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(0), VerticalAnchor.belowTop(0)))
+        );
+
     }
 
     @SuppressWarnings("SameParameterValue")
