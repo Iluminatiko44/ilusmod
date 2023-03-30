a085f14a769dc564b93ad6014749cc4620b06e35
Iluminatiko44 <ben.uhl@icloud.com>
2023-03-31 00:54:17 +0200

    abstracted more and fixed an issue, where the explosion didn't damage its owner

--- src/main/java/io/github/Iluminatiko44/ilusmod/custom/BulletEntity.java
+++ src/main/java/io/github/Iluminatiko44/ilusmod/custom/BulletEntity.java
@@ -8,26 +8,36 @@ import net.minecraft.world.phys.EntityHitResult;
 import org.jetbrains.annotations.NotNull;
 
 public class BulletEntity extends Arrow {
-    public BulletEntity(Level p_36858_, LivingEntity p_36859_) {
+    private final float expoRad;
+    public BulletEntity(Level p_36858_, LivingEntity p_36859_, float explosionRadius) {
         super(p_36858_, p_36859_);
+        this.expoRad = explosionRadius;
     }
 
     @Override
     protected void onHitBlock(@NotNull BlockHitResult p_36861_) {
-        explode(this.level, this.getX(), this.getY(), this.getZ(), (LivingEntity) this.getOwner());
+        explode(this);
         super.onHitBlock(p_36861_);
         this.remove(RemovalReason.DISCARDED);
     }
     @Override
     public void onHitEntity(@NotNull EntityHitResult p_36863_) {
-        explode(this.level, this.getX(), this.getY(), this.getZ(), (LivingEntity) this.getOwner());
+        explode(this);
         super.onHitEntity(p_36863_);
         this.remove(RemovalReason.DISCARDED);
     }
 
-    private static void explode(Level level, double x, double y, double z, LivingEntity owner) {
-                        // smt with mob explosion; x-, y- and z- Coordinates; explosion radius; ExplosionType. In this case TNT, because it's easy
-        level.explode(null, x, y, z, 15.0F, Level.ExplosionInteraction.TNT);
+    private static void explode(BulletEntity projectile) {
 
+        projectile.getLevel().explode(
+                projectile, // the projectile itself, because the explosion shouldn't damage its host. It also gets the owner from this.
+                projectile.getX(), projectile.getY(), projectile.getZ(), // x-, y- and z- Coordinates;
+                projectile.getExplosionRadius(), // Explosion radius;
+                Level.ExplosionInteraction.TNT // ExplosionType. In this case TNT, because it's easy
+        );
+    }
+
+    public float getExplosionRadius() {
+        return expoRad;
     }
 }
