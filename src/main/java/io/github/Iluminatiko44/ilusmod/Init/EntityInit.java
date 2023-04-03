79da1d6cbf597b072004c2307926e6721653fda6
Iluminatiko44 <ben.uhl@icloud.com>
2023-04-04 00:00:33 +0200

    registered the RocketEntity... kind of...

--- src/main/java/io/github/Iluminatiko44/ilusmod/Init/EntityInit.java
+++ src/main/java/io/github/Iluminatiko44/ilusmod/Init/EntityInit.java
@@ -1,12 +1,24 @@
 package io.github.Iluminatiko44.ilusmod.Init;
 
+import io.github.Iluminatiko44.ilusmod.Ilusmod;
+import io.github.Iluminatiko44.ilusmod.custom.rockets.RocketEntity;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class EntityInit {
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, Ilusmod.MODID);
+    public static final RegistryObject<EntityType<RocketEntity>> ROCKET =
+            ENTITY_TYPES.register(
+                    "rocket",
+                    () -> EntityType.Builder.<RocketEntity>
+                                    of(RocketEntity::new, MobCategory.MISC)
+                            .sized(0.25F, 0.25F)
+                            .clientTrackingRange(4)
+                            .updateInterval(20)
+                            .build(new ResourceLocation(Ilusmod.MODID, "rocket").toString()));
 
-    public static final DeferredRegister<EntityType<?>> ENTITYS = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, "ilusmod");
-
-    public static final EntityType<?> BULLET = null;
 }
