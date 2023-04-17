4c824bf07386a8def497c91936a93f21b475830f
Iluminatiko44 <ben.uhl@icloud.com>
2023-04-17 17:01:47 +0200

    Abstracted the rocketEntity for use of multiple rockets.

--- src/main/java/io/github/Iluminatiko44/ilusmod/Init/EntityInit.java
+++ src/main/java/io/github/Iluminatiko44/ilusmod/Init/EntityInit.java
@@ -1,10 +1,9 @@
 package io.github.Iluminatiko44.ilusmod.Init;
 
 import io.github.Iluminatiko44.ilusmod.Ilusmod;
-import io.github.Iluminatiko44.ilusmod.custom.rockets.RocketEntity;
 import io.github.Iluminatiko44.ilusmod.datagen.ModTagsProvider;
+import io.github.Iluminatiko44.ilusmod.entities.AbstractRocket;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.Level;
@@ -14,29 +13,28 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class EntityInit {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, Ilusmod.MODID);
-    private static final TagKey<EntityType<?>> ROCKETS = ModTagsProvider.EntityTypeTagsProvider.ROCKETS;
-    public static final RegistryObject<EntityType<RocketEntity>> ROCKET =
+    public static final RegistryObject<EntityType<AbstractRocket.Rocket>> ROCKET =
             ENTITY_TYPES.register("rocket", () -> EntityType.Builder.
-                    of((EntityType<RocketEntity> e, Level l) -> new RocketEntity.Rocket(e, l, ROCKETS),
+                    of((EntityType<AbstractRocket.Rocket> e, Level l) -> new AbstractRocket.Rocket(e, l),
                             MobCategory.MISC)
-                    .sized(0.25F, 0.25F)
+                    .sized(0.2F, 0.85F)
                     .clientTrackingRange(4)
                     .updateInterval(20)
                     .build(new ResourceLocation(Ilusmod.MODID, "rocket").toString()));
-    public static final RegistryObject<EntityType<RocketEntity>> NUKE_ROCKET =
+    public static final RegistryObject<EntityType<AbstractRocket.Nuke>> NUKE =
             ENTITY_TYPES.register("nuclear_rocket", () -> EntityType.Builder.
-                            of((EntityType<RocketEntity> e, Level l) -> new RocketEntity.Nuke(e, l, ROCKETS),
+                            of((EntityType<AbstractRocket.Nuke> e, Level l) -> new AbstractRocket.Nuke(e, l, ModTagsProvider.EntityTypeTagsProvider.ROCKETS),
                             MobCategory.MISC)
-                    .sized(0.5F, 0.5F)
+                    .sized(0.5F, 1.0F)
                     .clientTrackingRange(4)
                     .updateInterval(20)
                     .build(new ResourceLocation(Ilusmod.MODID, "nuclear_rocket").toString()));
-    public static final RegistryObject<EntityType<RocketEntity>> EXPLOSIVE_ROCKET =
+    public static final RegistryObject<EntityType<AbstractRocket.ExplosiveRocket>> EXPLOSIVE_ROCKET =
                                     // The name of the entity in the registry
             ENTITY_TYPES.register("explosive_rocket", () -> EntityType.Builder.
-                            of((EntityType<RocketEntity> e, Level l) -> new RocketEntity.ExplosiveRocket(e, l, ROCKETS),
+                            of((EntityType<AbstractRocket.ExplosiveRocket> e, Level l) -> new AbstractRocket.ExplosiveRocket(e, l, ModTagsProvider.EntityTypeTagsProvider.ROCKETS),
                             MobCategory.MISC)
-                    .sized(0.25F, 0.25F)
+                    .sized(0.2F, 0.85F)
                     .clientTrackingRange(4)
                     .updateInterval(20)
                     .build(new ResourceLocation(Ilusmod.MODID, "explosive_rocket").toString()));
